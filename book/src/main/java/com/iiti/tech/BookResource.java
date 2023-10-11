c5b10aeaaac7aa9d84f3ffc74093a4f3022e81de
Sonu Kumar Kushwaha <sksonukushwaha403@gmail.com>
2023-10-11 15:44:23 +0530

    implemented the fault tolerence using retry and fallback annotations

--- book/src/main/java/com/iiti/tech/BookResource.java
+++ book/src/main/java/com/iiti/tech/BookResource.java
@@ -3,10 +3,13 @@ package com.iiti.tech;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
+import org.eclipse.microprofile.faulttolerance.Fallback;
+import org.eclipse.microprofile.faulttolerance.Retry;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
 import org.jboss.logging.Logger;
 
 import java.time.Instant;
+import java.time.temporal.ChronoUnit;
 
 @Path("/api/books")
 public class BookResource {
@@ -22,6 +25,8 @@ public class BookResource {
     @POST
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    //@Retry(maxRetries = 4,delay = 2,delayUnit = ChronoUnit.SECONDS)
+    @Fallback(fallbackMethod = "fallcreate")
     public Book create(@FormParam("title") String title,@FormParam("author") String author, @FormParam("year") int yearOfPublication,@FormParam("genre") String genre){
         Book book=new Book();
         book.title=title;
@@ -34,6 +39,23 @@ public class BookResource {
 //and our book microservices will act as "rest client" for the isbn microservice
 // hence we need to create the proxy between a and b
         book.isbn13=proxy.generateIsbnNumbers().isbn13;
+        logger.info("book created : "+ book);
+        return book;
+
+    }
+
+    public Book fallcreate(@FormParam("title") String title,@FormParam("author") String author, @FormParam("year") int yearOfPublication,@FormParam("genre") String genre){
+        Book book=new Book();
+        book.title=title;
+        book.author=author;
+        book.yearOfPublication=yearOfPublication;
+        book.genre=genre;
+
+        book.creationDate= Instant.now();
+//here wrt to our client, book microservice acts as "proxy" that contacts the isbn microservice for isbn number
+//and our book microservices will act as "rest client" for the isbn microservice
+// hence we need to create the proxy between a and b
+        book.isbn13="will get back to you ";
         logger.info("book created : "+ book);
         return book;
 
